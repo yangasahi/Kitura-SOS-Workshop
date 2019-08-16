8cf0e5ce2ff5c37b6db8068266131049b85c66d2
Cameron McWilliam <cam.mcwilliam@me.com>
2019-08-16 15:32:55 +0100

    added model files master

---
+++ kitura-sos-dashboard/kitura-safe-lab-dashboard/Models.swift
@@ -0,0 +1,42 @@
+//
+//  Person.swift
+//  Application
+//
+//  Created by David Okun on 5/30/19.
+//
+
+import Foundation
+
+struct Safety: Codable {
+    var status: String
+}
+
+struct Coordinate: Codable, Hashable {
+    var latitude: Double
+    var longitude: Double
+}
+
+struct Person: Codable {
+    static func == (lhs: Person, rhs: Person) -> Bool {
+        return lhs.id == rhs.id
+    }
+
+    var coordinate: Coordinate
+    var name: String
+    var id: String
+    var status: Safety
+}
+
+struct Disaster: Codable {
+    var coordinate: Coordinate
+    var name: String
+}
+
+struct Dashboard: Codable {
+    var coordinate: Coordinate
+    var dashboardID: String
+}
+
+struct RegistrationToken: Codable {
+    var tokenID: String
+}
